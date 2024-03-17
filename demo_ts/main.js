70f7781ec8f01b4344a27699316239c67906430d
Noel Vaca <noel.vm.2501@gmail.com>
2024-03-16 22:15:06 -0400

    requerimientos, instalación, primeros pasos y tipos de datos en typescript

---
+++ demo_ts/main.js
@@ -0,0 +1,73 @@
+"use strict";
+// Esto es un comentario en una sóla línea
+/* Esto es un comentario
+  en múltiples líneas
+*/
+// Tipos de Datos
+// Boolean y ámbito de variables
+let esActivo = true; // En el contexto acual
+var esActivo2 = false; // El contexto es en todo el archivo
+const esActivo3 = false; // Constante de sólo lectura
+// Number
+let entero = 54;
+let real = 7.13;
+let binario = 0b101;
+let octal = 0o11;
+let hexadecimal = 0xa;
+let enteroLargo = 5465465465;
+// String
+let sigla = 'SIS257';
+let materia = 'Desarrollo de Aplicación Int/Internet II ';
+let concatenar = sigla + ' - ' + materia;
+let concatenar2 = `${sigla} - ${materia}`;
+let mayuscula = materia.toUpperCase();
+let minuscula = materia.toLowerCase();
+let reemplazo1raOcurrencia = materia.replace(' ', '');
+let reemplazoTodasOcurrencias = materia.replace(/ /g, '');
+let subcadena = materia.substring(0, 12);
+let repetir = sigla.repeat(5);
+let quitarEspaciosExtremos = materia.trim();
+let longitudCadena = quitarEspaciosExtremos.length;
+let separarCadena = materia.split(' ');
+// Array
+let array = ['uno', 'dos', 'tres'];
+let arrayNumerico = [1, 2, 3];
+let longitudArray = array.length;
+let doble = arrayNumerico.map((n) => n * 2);
+let factorial = arrayNumerico.reduce((prev, next) => prev * next);
+let impares = arrayNumerico.filter((n) => n % 2 != 0);
+array.push('cuatro');
+array.push('cinco');
+array.shift();
+array.pop();
+let nuevoArray = [];
+for (let i = 0; i < arrayNumerico.length; i++) {
+    nuevoArray.push(arrayNumerico[i] * 2);
+}
+// Tuple
+let tuple = [true, 50, 'SIS257', 30.56, [5, 6, 7]];
+// Enum
+var sexo;
+(function (sexo) {
+    sexo["M"] = "Masculino";
+    sexo["F"] = "Femenino";
+})(sexo || (sexo = {}));
+var color;
+(function (color) {
+    color["R"] = "Rojo";
+    color["G"] = "Verde";
+    color["Y"] = "Amarrillo";
+})(color || (color = {}));
+var tipo;
+(function (tipo) {
+    tipo[tipo["par"] = 0] = "par";
+    tipo[tipo["impar"] = 1] = "impar";
+})(tipo || (tipo = {}));
+// Any
+let cualquiera = 'HOLA';
+cualquiera = 5;
+cualquiera = undefined;
+let noDefinido;
+// Object
+let objeto = { nombre: 'Juan', apellido: 'Pérez' };
+console.log(objeto);
---
+++ demo_ts/main.ts
@@ -0,0 +1,69 @@
+// Esto es un comentario en una sóla línea
+/* Esto es un comentario
+  en múltiples líneas
+*/
+
+// Tipos de Datos
+// Boolean y ámbito de variables
+let esActivo: boolean = true; // En el contexto acual
+var esActivo2: boolean = false; // El contexto es en todo el archivo
+const esActivo3: boolean = false; // Constante de sólo lectura
+
+// Number
+let entero: number = 54;
+let real: number = 7.13;
+let binario: number = 0b101;
+let octal: number = 0o11;
+let hexadecimal: number = 0xa;
+let enteroLargo: number = 5465465465;
+
+// String
+let sigla: string = 'SIS257';
+let materia: string = 'Desarrollo de Aplicación Int/Internet II ';
+let concatenar: string = sigla + ' - ' + materia;
+let concatenar2: string = `${sigla} - ${materia}`;
+let mayuscula: string = materia.toUpperCase();
+let minuscula: string = materia.toLowerCase();
+let reemplazo1raOcurrencia: string = materia.replace(' ', '');
+let reemplazoTodasOcurrencias: string = materia.replace(/ /g, '');
+let subcadena: string = materia.substring(0, 12);
+let repetir: string = sigla.repeat(5);
+let quitarEspaciosExtremos: string = materia.trim();
+let longitudCadena: number = quitarEspaciosExtremos.length;
+let separarCadena: string[] = materia.split(' ');
+
+// Array
+let array: string[] = ['uno', 'dos', 'tres'];
+let arrayNumerico: number[] = [1, 2, 3];
+let longitudArray: number = array.length;
+let doble = arrayNumerico.map((n) => n * 2);
+let factorial = arrayNumerico.reduce((prev, next) => prev * next);
+let impares = arrayNumerico.filter((n) => n % 2 != 0);
+array.push('cuatro');
+array.push('cinco');
+array.shift();
+array.pop();
+
+let nuevoArray: number[] = [];
+for (let i = 0; i < arrayNumerico.length; i++) {
+  nuevoArray.push(arrayNumerico[i] * 2);
+}
+
+// Tuple
+let tuple = [true, 50, 'SIS257', 30.56, [5, 6, 7]];
+
+// Enum
+enum sexo { M = 'Masculino', F = 'Femenino' }
+enum color { R = 'Rojo', G = 'Verde', Y = 'Amarrillo' }
+enum tipo { par = 0, impar = 1 }
+
+// Any
+let cualquiera: any = 'HOLA';
+cualquiera = 5;
+cualquiera = undefined;
+let noDefinido;
+
+// Object
+let objeto: Object = { nombre: 'Juan', apellido: 'Pérez' };
+
+console.log(objeto);
